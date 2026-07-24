69cb5927389c5ca29dbc47002339baae16896a73
Hiago 7k <hiago7k@gmail.com>
2026-07-24 17:39:43 -0300

    add features

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,13 @@ class Main {
         System.out.println("2 - Agency Virtual ");
         Scanner data = new Scanner(System.in);
         int response = data.nextInt();
-
         System.out.println("Your type is confirm? " + response);
+
+        if (response == 2){
+            System.out.println("For continue your order acess this link: http.orange.bank/com.br");
+        }else {
+            System.out.println("What address is your agency? ");
+        }
+
     }
 }
