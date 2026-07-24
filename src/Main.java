5837f4353be4040827da64463464cdf5eee124cd
Hiago 7k <hiago7k@gmail.com>
2026-07-24 17:37:07 -0300

    strcture

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 class Main {
     static void main() {
-        System.out.println("orange banking");
+
+        System.out.println("---------------------------------");
+        System.out.println("**** Welcome to Orange Bank ****");
+        System.out.println("---------------------------------");
+
+        System.out.println("Please chose your necessity: ");
+        System.out.println("1 - Agency Physical ");
+        System.out.println("2 - Agency Virtual ");
+        Scanner data = new Scanner(System.in);
+        int response = data.nextInt();
+
+        System.out.println("Your type is confirm? " + response);
     }
 }
