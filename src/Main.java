7f3515e36c745d906040dd79a258b06c70228fea
Hiago 7k <hiago7k@gmail.com>
2026-07-25 12:43:30 -0300

    add while for looping in software

--- src/Main.java
+++ src/Main.java
@@ -7,25 +7,31 @@ class Main {
         System.out.println("**** Welcome to Orange Bank ****");
         System.out.println("---------------------------------");
 
-        System.out.println("Please chose your necessity: ");
-        System.out.println("1 - Agency Physical ");
-        System.out.println("2 - Agency Virtual ");
-        Scanner data = new Scanner(System.in);
-        int response = data.nextInt();
-        System.out.println("Your type is confirm? " + response);
+        int response = 1;
+        while(response != 0){
 
-        if (response == 2){
-            System.out.println("For continue your order acess this link: http.orange.bank/com.br");
-            System.out.println("Who can help you today? ");
-            System.out.println("1 - Check balance");
-            System.out.println("2 - make a deposit");
-            System.out.println("3 - make a withdraw");
-            System.out.println("4 - make a transaction");
-            System.out.println("5 - back to menu");
-            int secondResponse = data.nextInt();
-        }else {
-            System.out.println("What address is your agency? ");
+            System.out.println("Please chose your necessity: ");
+            System.out.println("1 - Agency Physical ");
+            System.out.println("2 - Agency Virtual ");
+            System.out.println("3 - Finance Investments");
+            System.out.println("0 - To leave");
+            Scanner data = new Scanner(System.in);
+            response = data.nextInt();
+
+
+            if (response == 2){
+                System.out.println("For continue your order access this link: http.orange.bank/com.br");
+            }else if(response == 1) {
+                System.out.println("Who can help you today? ");
+                System.out.println("1 - Check balance");
+                System.out.println("2 - make a deposit");
+                System.out.println("3 - make a withdraw");
+                System.out.println("4 - make a transaction");
+                System.out.println("0 - To Leave");
+                int secondResponse = data.nextInt();
+            }else {
+                break;
+            }
         }
-
     }
 }
